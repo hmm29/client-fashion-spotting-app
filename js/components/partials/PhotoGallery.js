ee98804508c90b3d2c7e9423f6dcd41b31101cb8
Harrison Miller <hmaxmiller@gmail.com>
2016-07-05 14:46:51 -0400

    start setting up photo gallery component

--- js/components/partials/PhotoGallery.js
+++ js/components/partials/PhotoGallery.js
@@ -5,3 +5,15 @@
  * @providesModule PhotoGallery
  * @flow
  */
+
+ const PhotoBrowser = require('react-native-photo-browser');
+
+ var PhotoGallery = React.createClass({
+    render() {
+
+    }
+ });
+
+ const styles = StyleSheet.create({
+
+ });
