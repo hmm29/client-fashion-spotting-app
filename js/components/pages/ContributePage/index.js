8f82e637e61a0226b24cb5e907f4effe02f26802
Harrison Miller <hmaxmiller@gmail.com>
2016-07-17 22:45:20 -0400

    reenable default filter icons during camera mode in COntributePage

--- js/components/pages/ContributePage/index.js
+++ js/components/pages/ContributePage/index.js
@@ -106,7 +106,7 @@ var ContributePage = React.createClass({
 
     const swiper = (
       <Swiper ref={SWIPER_REF}
-              index={2}
+              //index={2}
               style={styles.wrapper}
               bounces={false}
               scrollEnabled={false} // prevent touch scrolling
